ad66398d09bc9cb14ed4a4bb519968b4462cb0c1
kvnal <72142540+kvnal@users.noreply.github.com>
2021-03-25 17:41:13 +0530

    UI + code

--- formatter.js
+++ formatter.js
@@ -1,12 +1,41 @@
 
+
+const btn = document.querySelector("#btn")
 function format(){
-    var inp=document.getElementById("input").value;
-    var json=JSON.stringify(eval(inp));
+    var inp=document.getElementById("input").value.trim();
+    console.log(typeof inp);
+    if(inp==""){
+        return alert("no input given")
+    }
+    try{
+        var json=JSON.stringify(inp);
+        console.log(json);
+        console.log(typeof json);
+    }
+    catch(err){
+        //  alert("Wrong type")
+        console.log(err);
+        //  return window.location.reload()
+        
+    }
+
     document.getElementById("output").value=json;
+    copy();
+    btn.innerHTML="copied!" 
+    btn.classList.remove('bg-blue-500')
+    btn.classList.add('bg-green-400')
+    setTimeout(`
+    btn.classList.remove('bg-green-400')
+    btn.classList.add('bg-blue-500')
+    btn.innerHTML="Format" 
+    `,2000)
+    
 }
 
 function copy(){
     var json= document.getElementById("output");
     json.select();
-    return document.execCommand("copy");
+    // return 
+    document.execCommand("copy");
+    console.log("copied");
 }
